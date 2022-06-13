0e30250f4e7220361dc8aa4d6e85ec690f6316e8
Joey Drenth <drenthjoey@gmail.com>
2022-06-13 16:48:19 +0200

    Add base console command

---
+++ src/Console/IntegrateCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drenth1\TailwindIntegration\Console;
+
+use Illuminate\Console\Command;
+
+class IntegrateCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     * 
+     * @var string
+     */
+    protected $signature = 'tailwind:integrate';
+
+    /**
+     * The console command description.
+     * 
+     * @var string
+     */
+    protected $description = 'Set up the Tailwind resources';
+
+    /**
+     * Execute the console command.
+     * 
+     * @return void
+     */
+    public function handle()
+    {
+        //
+    }
+}
