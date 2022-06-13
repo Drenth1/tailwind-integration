0f61d5e94020356b94f07944b8263e15f83d1b3c
Joey Drenth <drenthjoey@gmail.com>
2022-06-13 16:12:00 +0200

    Add serviceprovider

---
+++ src/TailwindIntegrationServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drenth1\TailwindIntegration;
+
+use Illuminate\Support\ServiceProvider;
+
+class TailwindIntegrationServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the package services.
+     * 
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap the package services.
+     * 
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
