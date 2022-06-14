c56348ec7f88ab9493bbfbc89e28b8e6293907f1
Joey Drenth <drenthjoey@gmail.com>
2022-06-14 10:42:36 +0200

    Dynamically determine package path

--- src/Console/IntegrateCommand.php
+++ src/Console/IntegrateCommand.php
@@ -23,6 +23,13 @@ class IntegrateCommand extends Command
     protected $description = 'Set up the Tailwind resources';
 
     /**
+     * The package to integrate with.
+     * 
+     * @var string
+     */
+    protected static string $parent_package = 'drenth1/tailwind-integration';
+
+    /**
      * Execute the console command.
      * 
      * @return void
@@ -30,16 +37,28 @@ class IntegrateCommand extends Command
     public function handle()
     {
         $this->info('Running Tailwind integration.');
+        
+        $dir = self::packageDir();
 
         NodePackageManager::add();
 
-        AssetMover::moveAsset(__DIR__.'/../../resources/tailwind.config.js', base_path('tailwind.config.js'));
-        AssetMover::moveAsset(__DIR__.'/../../resources/webpack.config.js', base_path('webpack.config.js'));
+        AssetMover::moveAsset($dir . '/resources/tailwind.config.js', base_path('tailwind.config.js'));
+        AssetMover::moveAsset($dir . '/resources/webpack.config.js', base_path('webpack.config.js'));
 
-        AssetMover::moveAssetDirectory(__DIR__.'/../../resources/css', resource_path('css'));
-        AssetMover::moveAssetDirectory(__DIR__.'/../../resources/js', resource_path('js'));
-        AssetMover::moveAssetDirectory(__DIR__.'/../../resources/fonts', resource_path('fonts'));
+        AssetMover::moveAssetDirectory($dir . '/resources/css', resource_path('css'));
+        AssetMover::moveAssetDirectory($dir . '/resources/js', resource_path('js'));
+        AssetMover::moveAssetDirectory($dir . '/resources/fonts', resource_path('fonts'));
 
         $this->info('Tailwind integration complete, please run "npm install && npm run dev"');
+    }
+
+    /**
+     * Get the directory to the package to integrate with.
+     * 
+     * @return string
+     */
+    protected static function packageDir()
+    {
+        return base_path('vendor/') . self::$parent_package;
     }
 }
