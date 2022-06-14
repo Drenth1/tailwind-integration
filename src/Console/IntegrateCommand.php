68d9ea366dd93a7cafd11b0a003cc25ca6739f9c
Joey Drenth <drenthjoey@gmail.com>
2022-06-14 10:24:34 +0200

    Refactor integration command

--- src/Console/IntegrateCommand.php
+++ src/Console/IntegrateCommand.php
@@ -3,6 +3,8 @@
 namespace Drenth1\TailwindIntegration\Console;
 
 use Illuminate\Console\Command;
+use Drenth1\TailwindIntegration\Utilities\AssetMover;
+use Drenth1\TailwindIntegration\Utilities\NodePackageManager;
 
 class IntegrateCommand extends Command
 {
@@ -27,6 +29,17 @@ class IntegrateCommand extends Command
      */
     public function handle()
     {
-        //
+        $this->info('Running Tailwind integration.');
+
+        NodePackageManager::add();
+
+        AssetMover::moveAsset(__DIR__.'/../../resources/tailwind.config.js', base_path('tailwind.config.js'));
+        AssetMover::moveAsset(__DIR__.'/../../resources/webpack.config.js', base_path('webpack.config.js'));
+
+        AssetMover::moveAssetDirectory(__DIR__.'/../../resources/css', resource_path('css'));
+        AssetMover::moveAssetDirectory(__DIR__.'/../../resources/js', resource_path('js'));
+        AssetMover::moveAssetDirectory(__DIR__.'/../../resources/fonts', resource_path('fonts'));
+
+        $this->info('Tailwind integration complete, please run "npm install && npm run dev"');
     }
 }
